e4f253147a9e3a3244fecd4cc1ecdbd1470466f6
Krystian Romkowski <242510@edu.p.lodz.pl>
2023-04-12 01:22:56 +0200

    dfdggdf

--- Testy/DaneSphereTest.cs
+++ Testy/DaneSphereTest.cs
@@ -5,14 +5,32 @@ namespace Testy
     public class DaneSphereTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void constructorTest()
         {
             Sphere sphere = new Sphere(1, 2, 3);
             Assert.IsNotNull(sphere);
             Assert.AreEqual(sphere.Radius, 3);
             Assert.AreEqual(sphere.X, 1);
             Assert.AreEqual(sphere.Y, 2);
+<<<<<<< HEAD
+            sphere.X = 2;
+            Assert.AreEqual(sphere.X, 2);
+            sphere.Y = 3;
+            Assert.AreEqual(sphere.Y, 3);
+            sphere.Radius = 5;
+            Assert.AreEqual(sphere.Radius, 5);
+        }
+        [TestMethod]
+        public void speedTest()
+        {
+            Sphere sphere = new Sphere(1, 2, 3);
+            sphere.Yspeed = 1;
+            Assert.AreEqual(sphere.Yspeed, 1);
+            sphere.Xspeed = 1;  
+            Assert.AreEqual(sphere.Xspeed, 1);
+=======
             
+>>>>>>> e619218d596d9d649b95231b172134fe11ed1df4
         }
     }
 }
---
+++ Testy/LogicAPITest.cs
@@ -0,0 +1,28 @@
+﻿using Logika;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testy
+{
+    [TestClass]
+    public class LogicAPITests
+    {
+        [TestMethod]
+        public void TestCreatePlane()
+        {
+            AbstractLogicAPI api = AbstractLogicAPI.CreateAPI();
+            api.createPlane(100, 100, 5);
+            List<SphereLogic> sphereLogics = api.GetSphereLogics();
+            Assert.AreEqual(5, sphereLogics.Count);
+            // Test that all spheres are within the plane
+            foreach (SphereLogic sphereLogic in sphereLogics)
+            {
+                Assert.IsTrue(sphereLogic.X >= sphereLogic.Radius && sphereLogic.X <= 100 - sphereLogic.Radius);
+                Assert.IsTrue(sphereLogic.Y >= sphereLogic.Radius && sphereLogic.Y <= 100 - sphereLogic.Radius);
+            }
+        }
+    }
+}
